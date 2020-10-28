992561f5ba7cf67a701ecf3569c1ebde3c2640c3
Ian Garcia <ian.garcia@nn.cz>
2020-10-28 11:59:59 +0100

    adding code to repo

---
+++ src/components/Header/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import style from './header.css';
+import FontAwesome from 'react-fontawesome';
+import SideNav from './SideNav/sidenav';
+
+const Header = (props) => {
+
+    const getNavBars = () => (
+        <div className={style.bars}>
+            <FontAwesome name='bars'
+                onClick={props.onOpenNav}
+                style={{
+                    color: '#dfdfdf',
+                    padding: '10px',
+                    cursor: 'pointer'
+                }}
+            />
+        </div>
+    )
+    
+    const getLogo = () => (
+        <Link to="/" className={style.logo}>
+            <img alt="nba logo" src="/images/nba_logo.png"></img>
+        </Link>
+    )
+
+    return(
+        <header className={style.header}>
+            <SideNav {...props}/>
+            <div className={style.headerOpt}>
+                {getNavBars()}
+                {getLogo()}
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import Routes from './routes';
+
+
+const App = () =>{
+  return(
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
